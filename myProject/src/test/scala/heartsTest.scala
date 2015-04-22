c0c0ec9f4c2c0ca42e769075d53a0071c7175e67
pantozac <zachary.panto@usma.edu>
2015-04-22 14:39:33 -0400

    Milestone 2 and 3
    90% solutions for milestone 2 and 3 for AI session

--- myProject/src/test/scala/heartsTest.scala
+++ myProject/src/test/scala/heartsTest.scala
@@ -5,17 +5,6 @@ import org.scalatest.Matchers
 
 class heartsTest extends FunSpec with Matchers {
 
-  describe("A Player") {
-    it ("should have a name"){
-      val player= new Player("player1",0)
-      assert (player.name != null)
-    }
-    it ("should have a seat value less than 4"){
-      val player= new Player("player1",0)
-      assert (player.seat < 4)
-      }
-  }
-  
   describe ("a Deck"){
     describe("before a hand"){
       it("has 52 cards"){
@@ -46,58 +35,33 @@ class heartsTest extends FunSpec with Matchers {
   }
   
   describe("a player's Hand"){
-    it("belongs to a player"){
-      val hand=new Hand(null, null)
-      assert (hand.player !=null)
-    }
-    
+   
     describe("at the start of a hand"){
       it ("has 13 cards"){
-        val hand=new Hand(null,null)
-        assert (hand.cardArray.length==13)
+        val hand=new Hand(0)
+        assert (hand.cardSet.size==13)
       }
-      it ("has no repeated elements"){
-        val hand = new Hand(null, null)
-        var flag = true
-        var mover=Set[Card]()
-        for (item<-0 until hand.cardArray.length){
-            mover+=hand.play(item)
-        }
-        assert (mover.size==13)
-      }
+
     }
-    describe("during a turn"){
-      it ("plays a valid card"){
-        val hand=new Hand(null,null)
-        assert(hand.play(0).isValid)// right now will play the first card in the hand regardless,
-        //there are rules that govern play that would change which card is played.
-      }
+    
+
       it ("has the number of cards equal to 13-(the round number)"){
-        val hand=new Hand(null,null)
+        val hand=new Hand(0)
         val sim=new SimControll
-        var count=0
         val roundNumber=sim.round
-        val nullCard=new Card(-1,null)
-        for (card <-hand.cardArray){
-          if (card!=nullCard) count+=1
-        }
-        assert (roundNumber==(13-count))
+        assert (roundNumber==(13-hand.cardSet.size))
       }
       it ("does not contain cards that were just played"){
-        val hand=new Hand(null,null)
-        val test=hand.play(0)
-        assert(hand.cardArray(0)!=test)
+        val hand=new Hand(0)
+        val playArea=new PlayArea
+        val test=hand.play("Heart")
+        playArea.assignCard(hand.seat,test)
+        assert(!hand.cardSet.contains(playArea.cards(hand.seat)))
       }
-    }
     }
 //Deleted the discard pile because it can be logically worked around.
   
-  describe("the Table"){
-    it("has 4 players"){fail}
-    it("has 4 decks"){fail}
-    it ("has 4 discard piles"){fail}
-    it("has a play area"){fail}
-  }
+
   describe("a Card"){
     it("has a rank and a valid suit"){
       val card=new Card(-1, null)
@@ -106,7 +70,7 @@ class heartsTest extends FunSpec with Matchers {
   }
   describe("the play area"){//must have an array and a point value associated with it, lead suit?
     it("determines the index of the highest ranking card"){
-      val play= new PlayArea(0,null)
+      val play= new PlayArea
       var best= 0
       for (index<-0 until play.cards.length){
         if (play.cards(index).suit==play.lead){
@@ -116,7 +80,7 @@ class heartsTest extends FunSpec with Matchers {
       assert(best==play.max)
     }
     it("ensures all cards are valid"){
-      val play= new PlayArea(0,null)
+      val play= new PlayArea
       var flag=true
       for (card<-play.cards){
         if (!card.isValid) flag=false
@@ -124,26 +88,55 @@ class heartsTest extends FunSpec with Matchers {
       assert(flag)
     }
     it("Tracks the points acquired in the pot"){
-      val play= new PlayArea(0,null)
-      assert (play.score>=0)
+      val play= new PlayArea
+      assert (play.calculateScore>=0)
     }
   }
   describe("a Scoreboard"){
-    it("carries scores over between rounds"){fail}
-    it("carries scores over between hands"){fail}
-    it("initializes at zero"){fail}
+    it("checks for a loser"){
+      val score =new Scoreboard
+      score.loserCheck should be (false)
+      score.scores(0)=100
+      score.loserCheck should be (true)
+    }
+    it("initializes at zero"){
+      val score =new Scoreboard
+      for (item<-score.scores) item should be (0)
+    }
   }
   describe("turn queue"){
     it("analyzes each player's hand for who has the 2 of clubs at the beginning of the game"){fail}
-    it("tracks which player took the pile in the previous round"){fail}
-    it("calculates player order based on who took the pile last/who has the 2 of clubs"){fail}
+    it("tracks which player took the pile in the previous round"){
+      val sim=new SimControll
+      sim.hand1.cardSet.apply(sim.twoClub)
+      sim.loadQueue
+      sim.queue.dequeue should be (0)
+      
+    }
+    it("calculates player order based on who took the pile last/who has the 2 of clubs"){
+      val test=new TurnQueue
+      test.load(0)
+      test.dequeue should be (0)
+      test.dequeue should be (1)
+      test.dequeue should be (2)
+      test.dequeue should be (3)
+      test.load (1)
+      test.dequeue should be (1)
+      test.dequeue should be (2)
+      test.dequeue should be (3)
+      test.dequeue should be (0)
+      test.load(2)
+      test.dequeue should be (2)
+      test.dequeue should be (3)
+      test.dequeue should be (0)
+      test.dequeue should be (1)
+    }
     it("determines which player is to the left of a given player"){fail}
     it("determines which player is to the right of a given player"){fail}
     it("determines which player is across from a given player"){fail}
+    //bottom three test cases can be solved mathmatically, and are not important for version 1.1
   }
   describe("the Mover"){
-    it("tracks whose turn it is"){fail}
-    it("determines when a player's turn is complete"){fail}
     it("advances the player order"){fail}
     it("tracks when the round is complete"){fail}
     it("gives the pile to the appropriate player"){fail}
